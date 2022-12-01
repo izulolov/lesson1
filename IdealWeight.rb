76e9030fd5aec936c010faaeeaaa2a34100b5f5c
izulolov <izulolov@mail.ru>
2022-12-01 23:34:17 +0300

    first commit

---
+++ IdealWeight.rb
@@ -0,0 +1,13 @@
+puts "Введите ваше имя:"
+name = gets.chomp
+
+puts "Введите ваш вес:"
+weight = gets.chomp.to_f
+
+if(weight-110*1.15 < 0)
+    puts "#{name}, Ваш вес уже оптимальный"
+else puts "#{name},Ваш вес не оптимальный :("
+
+end
+
+#puts (weight-110)*1.15 < 0 ? "#{name}, Ваш вес уже оптимальный" : "#{name}, Ваш вес не оптимальный :("
